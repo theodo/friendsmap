c639d4235a71a6514924f8bb3b490c227bf529ca
jbeurel <bejonster@gmail.com>
2012-09-12 16:57:37 +0200

    Init GoogleMaps

--- src/Theodo/MapBundle/Controller/DefaultController.php
+++ src/Theodo/MapBundle/Controller/DefaultController.php
@@ -9,9 +9,13 @@ class DefaultController extends Controller
     public function indexAction()
     {
     	$facebookApi = $this->get('fos_facebook.api');
+    	$user = $facebookApi->api('/me');
     	$friends = $facebookApi->api('/me/friends?fields=id,name,location');
     	$friends = $friends['data'];
 
-        return $this->render('TheodoMapBundle:Default:index.html.twig');
+
+        return $this->render('TheodoMapBundle:Default:index.html.twig', array(
+        	'user' => $user,
+        ));
     }
 }
