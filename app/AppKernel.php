8e57f533b0057180fe9a3c1f7a349e78c1fa5f62
jbeurel <bejonster@gmail.com>
2012-09-12 16:10:56 +0200

    FOSFacebookBundle Configuration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new FOS\FacebookBundle\FOSFacebookBundle(),
             new Theodo\MapBundle\TheodoMapBundle(),
         );
 
--- src/Theodo/MapBundle/Controller/DefaultController.php
+++ src/Theodo/MapBundle/Controller/DefaultController.php
@@ -8,6 +8,10 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+    	$facebookApi = $this->get('fos_facebook.api');
+    	$friends = $facebookApi->api('/me/friends?fields=id,name,location');
+    	$friends = $friends['data'];
+
         return $this->render('TheodoMapBundle:Default:index.html.twig');
     }
 }
