6d513fdc3c99a474caa9d93c937f4c3e03e9b159
jbeurel <bejonster@gmail.com>
2012-09-14 16:19:25 +0200

    Authentication with redirection to Facebook instead of js popup

--- src/Theodo/MapBundle/Controller/DefaultController.php
+++ src/Theodo/MapBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Theodo\MapBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
